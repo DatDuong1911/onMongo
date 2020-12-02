b2a54059214ea0918fae03b3c3d2f7467446d3dd
DatDuong1911 <datduong1911@gmail.com>
2020-12-03 00:36:08 +0700

    nop bai tap

--- router/user.js
+++ router/user.js
@@ -13,6 +13,7 @@ router.get("/", function(req, res) {
 router.post("/signup", function(req, res) {
     var username = req.body.username;
     var password = req.body.password;
+
     var age = req.body.age;
     var address = req.body.address;
     userService.signup(username, password, age, address).then(function(result) {
@@ -28,4 +29,44 @@ router.post("/signup", function(req, res) {
         })
     })
 })
+
+router.get("/:username", (req, res) => {
+    var username = req.params.username
+    userService.getUser(username)
+        .then(function(data) {
+            if (data.lenght !== 0) {
+                res.json(data)
+            } else {
+                res.json("Khong co nguoi dung")
+            }
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+})
+
+router.put("/:age", (req, res) => {
+    let ageparams = req.params.age
+    let username = req.body.username;
+    let password = req.body.password;
+    let age = req.body.age;
+    let address = req.body.address
+    userService.updateUser(ageparams, username, password, age, address)
+        .then((data) => {
+            res.json(data)
+        })
+        .catch((err) => {
+            res.json(err)
+        })
+})
+
+router.delete("/:address", function(req, res) {
+    var address = req.params.address
+    userService.delete(address).then(function(data) {
+        res.json(data)
+    }).catch(function(err) {
+        res.json(err)
+    })
+})
+
 module.exports = router
--- services/userService.js
+++ services/userService.js
@@ -4,6 +4,14 @@ var userService = {};
 userService.getAllUser = function() {
     return UserModel.find()
 }
+
+
+userService.getUser = function(username) {
+    return UserModel.find({
+        username: username
+    })
+};
+
 userService.signup = function(username, password, age, address) {
     return UserModel.create({
         username: username,
@@ -13,9 +21,10 @@ userService.signup = function(username, password, age, address) {
     })
 };
 
-userService.updateUser = function(usernameparams, username, password, age, address) {
-    return UserModel.updateMany({
-        username: usernameparams
+
+userService.updateUser = function(ageparams, username, password, age, address) {
+    return UserModel.updateOne({
+        age: ageparams
     }, {
         username: username,
         password: password,
@@ -23,6 +32,10 @@ userService.updateUser = function(usernameparams, username, password, age, addre
         address: address,
     })
 };
-
+userService.delete = function(address) {
+    return UserModel.deleteOne({
+        address: address
+    })
+};
 
 module.exports = userService
